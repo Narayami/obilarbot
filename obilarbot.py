03d57ac1d96098cb5622d208cc014121da26cf78
caralho <caralho@caralho.com>
2022-04-02 20:25:01 +0100

    aaa

--- obilarbot.py
+++ obilarbot.py
@@ -4,13 +4,13 @@ from telegram.ext import Updater, CommandHandler, CallbackContext, InlineQueryHa
 import requests
 import re
 import os
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 PORT = int(os.environ.get('PORT', 8443))
-TOKEN = '5137857279:AAGEWytqKXAhMU2iHFeO4U_7zDAucNyU1OE'
-updater = Updater(TOKEN)
-
-def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 def get_url():
     contents = requests.get('https://random.dog/woof.json').json()
@@ -55,8 +55,15 @@ def poko(update, context):
     chat_id = update.message.chat_id
     context.bot.send_photo(chat_id, 'https://imgur.com/LQ0uiwX')
 
+def error(update, context):
+    """Logs Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 def main():
-    updater.dispatcher.add_handler(CommandHandler('hello', hello))
+    TOKEN = '5137857279:AAGEWytqKXAhMU2iHFeO4U_7zDAucNyU1OE'
+    APP_NAME='https://obilarbot.herokuapp.com/'
+    updater = Updater(TOKEN, use_context=True)
+
     updater.dispatcher.add_handler(CommandHandler('dog', dog))
     updater.dispatcher.add_handler(CommandHandler('zatcho', zatcho))
     updater.dispatcher.add_handler(CommandHandler('snake', snake))
@@ -65,11 +72,10 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('elguaranapo', elguaranapo))
     updater.dispatcher.add_handler(CommandHandler('poko', poko))
 
-    updater.start_webhook(listen="0.0.0.0",
-                          port=PORT,
-                          url_path=TOKEN,
-                          webhook_url="https://obilarbot.herokuapp.com/" + TOKEN)
-    updater.start_polling()
+
+    updater.dispatcher.add_error_handler(error)
+
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN, webhook_url=APP_NAME + TOKEN)
     updater.idle()
 
 if __name__ == '__main__':
